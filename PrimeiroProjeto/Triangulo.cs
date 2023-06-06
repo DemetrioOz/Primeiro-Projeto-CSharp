a3fe39636fb95477405112da6d6c1f0b827f6f01
Matheus Demetrio Dama <matheusdama@ext.cvccorp.com.br>
2023-06-06 20:52:04 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/Triangulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        public double Area()
+        {
+            double p = this.p(this.A, this.B, this.C);
+            return Math.Sqrt(p * (p - this.A) * (p - this.A) * (p - this.A));
+        }
+
+        private double p(double a, double b, double c)
+        {
+            return (a + b + c) / 2.0;
+        }
+    }
+}
+
